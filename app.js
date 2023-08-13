9431000b79317bee68581689bb5aeb87ac5b871d
Sherif Okunade <o.d.alsherif@gmail.com>
2023-08-13 13:41:00 -0700

    Update cors

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const userRoutes = require('./routes/user');
 const booksRoute = require('./routes/books');
 const authMiddleware = require('./middleware/auth')
 const PORT = process.env.PORT || 8000;
+const cors = require('cors');
+
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user',authMiddleware, userRoutes);
 app.use('/api/books',authMiddleware, booksRoute);
+app.use(cors());
 
 mongoose.connect(process.env.DBSTRING)
   .then(()=>console.log('connected to mongoDb')).then(() => {
